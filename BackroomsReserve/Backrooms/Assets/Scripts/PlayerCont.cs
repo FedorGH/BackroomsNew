7fadf53cecf19d32934887a6f1d5d235f737b582
FedorGH <fedorgubarev07@gmail.com>
2023-05-20 12:05:28 +0300

    Backrooms v.0.0.0.9
    lvl 0

--- BackroomsReserve/Backrooms/Assets/Scripts/PlayerCont.cs
+++ BackroomsReserve/Backrooms/Assets/Scripts/PlayerCont.cs
@@ -4,30 +4,39 @@ using UnityEngine;
 
 public class PlayerCont : MonoBehaviour
 {
-    public float movementSpeed = 5f;
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
     public float mouseSensitivity = 2f;
+    public float jumpForce = 5f;
+    public float gravityMultiplier = 2f; // Увеличение притяжения
     public Camera playerCamera;
 
     private CharacterController characterController;
     private float verticalRotation = 0f;
+    private float verticalVelocity = 0f;
+    private bool isJumping = false;
+    private bool isRunning = false;
 
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked; // Скрываем указатель мыши и удерживаем его в центре экрана
+        Cursor.lockState = CursorLockMode.Locked;
         characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
         // Ввод с клавиатуры
-        float moveForward = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-        float moveSideways = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        float moveForward = Input.GetAxis("Vertical");
+        float moveSideways = Input.GetAxis("Horizontal");
+        float movementSpeed = isRunning ? runSpeed : walkSpeed;
+        moveForward *= movementSpeed * Time.deltaTime;
+        moveSideways *= movementSpeed * Time.deltaTime;
 
         // Вращение с помощью мыши
         float mouseHorizontal = Input.GetAxis("Mouse X") * mouseSensitivity;
         float mouseVertical = Input.GetAxis("Mouse Y") * mouseSensitivity;
         verticalRotation -= mouseVertical;
-        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f); // Ограничиваем угол вращения вверх и вниз
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
 
         // Применяем вращение к камере игрока
         playerCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
@@ -35,6 +44,38 @@ public class PlayerCont : MonoBehaviour
 
         // Движение персонажа
         Vector3 moveDirection = transform.forward * moveForward + transform.right * moveSideways;
+
+        // Проверяем возможность прыжка и обрабатываем его
+        if (characterController.isGrounded)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                isJumping = true;
+                verticalVelocity = jumpForce;
+            }
+            else
+            {
+                isJumping = false;
+                verticalVelocity = -0.5f;
+            }
+        }
+        else
+        {
+            // Увеличение притяжения
+            verticalVelocity += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
+        }
+
+        moveDirection.y = verticalVelocity * Time.deltaTime;
         characterController.Move(moveDirection);
+
+        // Проверяем состояние бега
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isRunning = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isRunning = false;
+        }
     }
 }
